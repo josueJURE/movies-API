234d87cfee2a36276c5b024c1552c2086070491c
josueJURE <josue.jure@gmail.com>
2023-05-01 19:07:27 +0100

    add line of code to swipeRight so that a differnt picture's displayed each user clicks on element with class chevronRight

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const chevronRight = document.querySelector(".fa-chevron-right");
-const chevronLeft = document.querySelector(".fa-chevron-left");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "868db2c1eamshd290e20c8dc8261p1627dejsnf52bb1929fb7",
+    "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+  },
+};
 
+fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
+  .then((response) => response.json())
+  .then((response) => {
+    const mainContainer = document.querySelector(".mainContainer");
+    const chevronRight = document.querySelector(".fa-chevron-right");
+    const chevronLeft = document.querySelector(".fa-chevron-left");
+    const img = document.querySelector(".carousel img");
+    let counting = 0;
+    img.src = response[counting].image;
+    console.log("josue");
 
-chevronRight.addEventListener("click", swipeRight);
+    chevronRight.addEventListener("click", swipeRight);
+    chevronLeft.addEventListener("click", swipeLeft);
 
+    function swipeLeft() {
+      console.log("hello world");
+    }
 
-function swipeRight() {
-    console.log("swip right")
-}
+ 
 
-// const mainContainer = document.querySelector('.mainContainer');
-// const ul = document.querySelector("ul");
-// console.log(ul.children)
-// console.log(mainContainer)
+    function swipeRight() {
+      counting++;
+      console.log(counting)
+      img.src = response[counting].image;
+    }
+  })
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '868db2c1eamshd290e20c8dc8261p1627dejsnf52bb1929fb7',
-// 		'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+  .catch((err) => console.error(err));
