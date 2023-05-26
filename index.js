26216e3d04c3f0914580d1c8f151b3c526003481
josueJURE <josue.jure@gmail.com>
2023-05-26 17:00:16 +0100

    add class watchList to li elment on searchByDirector.html so as to navigate ffrom and to any page

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
     const genre = document.querySelector(".genre");
     const rating = document.querySelector(".rating");
     const description = document.querySelector(".description");
-    const typesOfgenre = document.querySelector(".typesOfgenre");
+    const typesOfgenre  = document.querySelector(".typesOfgenre ");
     const selectByDirectors = document.querySelector(".selectByDirectors");
     const selectByGenres = document.querySelector(".selectByGenres");
     let trailerSection = document.querySelector(".trailerSection source");
@@ -67,14 +67,14 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
         directorsNameListNoDuplicates.forEach((director) => {
           const element = document.createElement("div");
           element.innerHTML = director;
-          typesOfgenre.appendChild(element);
+          typesOfgenre .appendChild(element);
         });
         isListEmpty = false;
       }
 
       // else hide list
       else {
-        typesOfgenre.innerHTML = "";
+        typesOfgenre .innerHTML = "";
         isListEmpty = true;
       }
     }
@@ -88,8 +88,8 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
     }
 
     function userSelectByGenre(e) {
-      if (typesOfgenre) {
-        typesOfgenre.innerHTML = "";
+      if (typesOfgenre ) {
+        typesOfgenre .innerHTML = "";
         const selectedGenre = e.target.value;
         response
           .filter((res) => res.genre.includes(selectedGenre))
@@ -106,7 +106,7 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
             img.src = movie.image;
             img.title = movie.description;
             element.appendChild(img);
-            typesOfgenre.appendChild(element);
+            typesOfgenre .appendChild(element);
           });
       }
     }
@@ -125,126 +125,7 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
         element.appendChild(img);
         userWatchlistParentElement.appendChild(element);
       });
-      const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": window.IMDB_API_KEY,
-    "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
-  },
-};
-
-const DEBUG = true;
-
-fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
-  .then((response) => response.json())
-  .then((response) => {
-    const chevronRight = document.querySelector(".fa-chevron-right");
-    const chevronLeft = document.querySelector(".fa-chevron-left");
-    const img = document.querySelector(".carousel img");
-    const title = document.querySelector(".title");
-    const director = document.querySelector(".director");
-    const genre = document.querySelector(".genre");
-    const rating = document.querySelector(".rating");
-    const description = document.querySelector(".description");
-    const directors = document.querySelector(".directors");
-    const selectByDirectors = document.querySelector(".selectByDirectors");
-    const selectByGenres = document.querySelector(".selectByGenres");
-    let trailerSection = document.querySelector(".trailerSection source");
-    const trailerButton = document.querySelector(".trailerButton");
-    const searchByGenre = document.querySelector(".searchByGenre");
-    const searchByDirectors = document.querySelector(".searchByDirectors");
-    const home = document.querySelector(".home");
-    const watchList = document.querySelector(".watchList");
-    const userWatchlistParentElement = document.querySelector(
-      ".userWatchlistParentElement"
-    );
-
-    console.log({ director: selectByDirectors }, { genre: selectByGenres });
-    console.log(response);
-
-    let directorsNameListWithDuplicates = [];
-    let genreListWithDuplicates = [];
-
-    // can this be refactored into a funcion
-    response.forEach((item) => {
-      directorsNameListWithDuplicates.push(...item.director);
-    });
-
-    response.forEach((item) => {
-      genreListWithDuplicates.push(...item.genre);
-    });
-
-    let directorsNameListNoDuplicates = [
-      ...new Set(directorsNameListWithDuplicates),
-    ];
-
-    console.log(directorsNameListNoDuplicates);
-
- 
-
-    let genreListWithNoDuplicates = [
-      ...new Set(genreListWithDuplicates),
-    ].sort();
-
-    console.log(genreListWithNoDuplicates);
-
-    let isListEmpty = true;
-
-    function showDirectors() {
-      // if (section is empty) show list
-      if (isListEmpty) {
-        directorsNameListNoDuplicates.forEach((director) => {
-          const element = document.createElement("div");
-          element.innerHTML = director;
-          directors.appendChild(element);
-        });
-        isListEmpty = false;
-      }
-
-      // else hide list
-      else {
-        directors.innerHTML = "";
-        isListEmpty = true;
-      }
     }
-
-    function displayGenres() {
-      genreListWithNoDuplicates.forEach((genre) => {
-        const element = document.createElement("option");
-        element.innerHTML = genre;
-        selectByGenres.appendChild(element);
-      });
-    }
-
-    function userSelectByGenre(e) {
-      if (directors) {
-        directors.innerHTML = "";
-        const selectedGenre = e.target.value;
-        response
-          .filter((res) => res.genre.includes(selectedGenre))
-          .forEach((movie) => {
-            const element = document.createElement("div");
-            const img = document.createElement("img");
-            img.addEventListener("click", function () {
-              const tasks = getTasksFromLocalStorage();
-              if (tasks.filter((task) => task.id == movie.id).length == 0) {
-                tasks.push(movie);
-                localStorage.setItem("tasks", JSON.stringify(tasks));
-              }
-            });
-            img.src = movie.image;
-            img.title = movie.description;
-            element.appendChild(img);
-            directors.appendChild(element);
-          });
-      }
-    }
-
-    if (userWatchlistParentElement) {
-      addMovieToUserWatchList();
-    }
-
-
 
     let counting = 0;
 
@@ -274,8 +155,13 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
       });
     }
 
+
+
+
+
     if (searchByDirectors) {
       searchByDirectors.addEventListener("click", function () {
+        console.log("directors")
         window.location.assign("/searchByDirectors.html");
       });
     }
@@ -342,121 +228,6 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", options)
       }
 
       return JSON.parse(localStorage.getItem("tasks") || "[]");
-    }
-    populateFields();
-  });
-    }
-
-    let counting = 0;
-
-    if (chevronRight) {
-      chevronRight.addEventListener("click", swipeRight);
-    }
-    if (chevronLeft) {
-      chevronLeft.addEventListener("click", swipeLeft);
-    }
-
-    if (selectByDirectors) {
-      selectByDirectors.addEventListener("click", showDirectors);
-    }
-
-    if (searchByGenre) {
-      searchByGenre.addEventListener("click", goToSearchByGenrePage);
-    }
-
-    if (selectByGenres) {
-      selectByGenres.addEventListener("change", userSelectByGenre);
-      displayGenres();
-    }
-
-    if (home) {
-      home.addEventListener("click", function () {
-        window.location.assign("/index.html");
-      });
-    }
-
-    if (searchByDirectors) {
-      searchByDirectors.addEventListener("click", function () {
-        window.location.assign("/searchByDirectors.html");
-      });
-    }
-
-    function goToSearchByGenrePage() {
-      console.log("genre");
-      window.location.assign("/searchByGenrePage.html");
-    }
-
-    if (watchList) {
-      watchList.addEventListener("click", function () {
-        window.location.assign("/watchList.html");
-      });
-    }
-
-    function swipeLeft() {
-      counting--;
-      if (counting < 0) {
-        counting = 99;
-      }
-      console.log(counting);
-      populateFields();
-    }
-
-    function swipeRight() {
-      counting++;
-      if (counting > 99) {
-        counting = 0;
-      }
-      populateFields();
-    }
-
-    function populateFields() {
-      if (img) {
-        img.src = response[counting].image;
-        director.innerHTML = `Director: ${response[counting].director}`;
-        genre.innerHTML = `Genre: ${response[counting].genre}`;
-        rating.innerHTML = `Rating ${response[counting].rating}`;
-        title.innerHTML = response[counting].title;
-        description.innerHTML = response[counting].description;
-      }
-    }
-
-    if (trailerButton) {
-      trailerButton.addEventListener("click", () => {
-        Swal.fire({
-          title: response[counting].title,
-          html: `<iframe id="ytplayer" type="text/html" width="100%" height="360"
-          src="${response[counting].trailer}?autoplay=1"
-          frameborder="0"></iframe>`,
-          width: "",
-          showCloseButton: true,
-          showConfirmButton: false,
-          buttonsStyling: true,
-          customClass: {
-            popup: "trailerPopup",
-          },
-        });
-      });
-    }
-    function getTasksFromLocalStorage() {
-      if (DEBUG) {
-        console.log("getTasksFromLocalStorage");
-      }
-
-      return JSON.parse(localStorage.getItem("tasks") || "[]");
-    }
-    function addMovieToUserWatchList() {
-      const tasks = getTasksFromLocalStorage();
-      tasks.forEach((task) => {
-        const element = document.createElement("div");
-        const img = document.createElement("img");
-        const button = document.createElement("button")
-        img.src = task.image;
-        img.title = task.description;
-        element.appendChild(img);
-        element.appendChild(button)
-        userWatchlistParentElement.appendChild(element);
-      });
-      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
     populateFields();
   });
